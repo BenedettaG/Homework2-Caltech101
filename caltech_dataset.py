dcfb1705e6aa520c9f3b58ed1bfbd5714cdcbd70
BenedettaG <66787137+BenedettaG@users.noreply.github.com>
2021-04-18 18:42:28 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -5,8 +5,10 @@ from PIL import Image
 import os
 import os.path
 import sys
-
-
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
@@ -15,32 +17,42 @@ def pil_loader(path):
 
 
 class Caltech(VisionDataset):
-    def __init__(self, root, split='train', transform=None, target_transform=None):
-        super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
+    def init(self, root, split='train', transform=None, target_transform=None):
+        super(Caltech, self).init(root, transform=transform, target_transform=target_transform)
 
         self.split = split # This defines the split you are going to use
                            # (split files are called 'train.txt' and 'test.txt')
+
+
+        self.root = root+"/"+split+".txt" #./train.txt
+        self.caltech_frame = pd.read_csv(self.root, delimiter = '\n', header=None)
+        self.transform = transform
+        self.target_transform = target_transform
 
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
         - If the RAM size allows it, it is faster to store all data in memory
         - PyTorch Dataset classes use indexes to read elements
-        - You should provide a way for the __getitem__ method to access the image-label pair
+        - You should provide a way for the getitem method to access the image-label pair
           through the index
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
 
-    def __getitem__(self, index):
+    def getitem(self, index):
         '''
-        __getitem__ should access an element through its index
+        getitem should access an element through its index
         Args:
             index (int): Index
-
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
         '''
-
-        image, label = ... # Provide a way to access image and label via index
+        img_name = os.path.join(self.root, self.caltech_frame.iloc[index, 0])
+        label = img_name.split("/")[3]
+        name = img_name.split("/")[4]
+        image = io.imread("./101_ObjectCategories/"+"/"+label+"/"+name)
+        
+        image = Image.fromarray(image)
+        sample = {'image' : image, 'label' : label} # Provide a way to access image and label via index
                            # Image should be a PIL Image
                            # label can be int
 
@@ -48,12 +60,13 @@ class Caltech(VisionDataset):
         if self.transform is not None:
             image = self.transform(image)
 
-        return image, label
+        return sample
 
-    def __len__(self):
+    def len(self):
         '''
-        The __len__ method returns the length of the dataset
+        The len method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-        length = ... # Provide a way to get the length (number of elements) of the dataset
+
+        length = self.caltech_frame.size # Provide a way to get the length (number of elements) of the dataset
         return length
